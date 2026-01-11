80685ec58e66c419330ba9e93ec5460103b8d187
cjamison <elocjam13@gmail.com>
2026-01-10 23:07:57 -0500

    Created error handling, refactored Poll.java, created messages.properties

--- src/main/java/io/zipcoder/tc_spring_poll_application/domain/Poll.java
+++ src/main/java/io/zipcoder/tc_spring_poll_application/domain/Poll.java
@@ -1,6 +1,8 @@
 package io.zipcoder.tc_spring_poll_application.domain;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.Set;
 
 @Entity
@@ -12,20 +14,22 @@ public class Poll {
     private Long id;
     
     @Column(name = "QUESTION")
+    @NotEmpty
     private String question;
     
     @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "POLL_ID")
     @OrderBy
+    @Size(min=2, max=6)
     private Set<Option> options;
-
+    
     public Poll() {
     }
     
     public Poll(String question) {
         this.question = question;
     }
-
+    
     public Long getId() {
         return id;
     }

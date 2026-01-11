82a13a61d1d8002a77dd5a8a6c50ee9fa973fe38
cjamison <elocjam13@gmail.com>
2026-01-10 23:00:52 -0500

    Refactored Poll Controller, updated POM file to accept Valid import

--- src/main/java/io/zipcoder/tc_spring_poll_application/controllers/PollController.java
+++ src/main/java/io/zipcoder/tc_spring_poll_application/controllers/PollController.java
@@ -1,6 +1,7 @@
 package io.zipcoder.tc_spring_poll_application.controllers;
 
 import io.zipcoder.tc_spring_poll_application.domain.Poll;
+import io.zipcoder.tc_spring_poll_application.exception.ResourceNotFoundException;
 import io.zipcoder.tc_spring_poll_application.repositories.PollRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.net.URI;
 
 @RestController
@@ -16,15 +18,21 @@ public class PollController {
     
     @Autowired
     private PollRepository pollRepository;
-
+    
+    private void verifyPoll(Long pollId) {
+        if (!pollRepository.existsById(pollId)) {
+            throw new ResourceNotFoundException("Poll with ID " + pollId + " not found");
+        }
+    }
+    
     @RequestMapping(value="/polls", method=RequestMethod.GET)
     public ResponseEntity<Iterable<Poll>> getAllPolls() {
         Iterable<Poll> allPolls = pollRepository.findAll();
         return new ResponseEntity<>(allPolls, HttpStatus.OK);
     }
-
+    
     @RequestMapping(value="/polls", method=RequestMethod.POST)
-    public ResponseEntity<?> createPoll(@RequestBody Poll poll) {
+    public ResponseEntity<?> createPoll(@Valid @RequestBody Poll poll) {
         poll = pollRepository.save(poll);
         
         URI newPollUri = ServletUriComponentsBuilder
@@ -38,23 +46,26 @@ public class PollController {
         
         return new ResponseEntity<>(headers, HttpStatus.CREATED);
     }
-
+    
     @RequestMapping(value="/polls/{pollId}", method=RequestMethod.GET)
     public ResponseEntity<?> getPoll(@PathVariable Long pollId) {
+        verifyPoll(pollId);
         return pollRepository.findById(pollId)
             .map(poll -> new ResponseEntity<>(poll, HttpStatus.OK))
             .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
-
+    
     @RequestMapping(value="/polls/{pollId}", method=RequestMethod.PUT)
-    public ResponseEntity<?> updatePoll(@PathVariable Long pollId, @RequestBody Poll poll) {
+    public ResponseEntity<?> updatePoll(@PathVariable Long pollId, @Valid @RequestBody Poll poll) {
+        verifyPoll(pollId);
         poll.setId(pollId);
         pollRepository.save(poll);
         return new ResponseEntity<>(HttpStatus.OK);
     }
-
+    
     @RequestMapping(value="/polls/{pollId}", method=RequestMethod.DELETE)
     public ResponseEntity<?> deletePoll(@PathVariable Long pollId) {
+        verifyPoll(pollId);
         pollRepository.deleteById(pollId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
---
+++ src/main/java/io/zipcoder/tc_spring_poll_application/exception/ResourceNotFoundException.java
@@ -0,0 +1,18 @@
+package io.zipcoder.tc_spring_poll_application.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    
+    private static final long serialVersionUID = 1L;
+    
+    public ResourceNotFoundException() {
+        super();
+    }
+    
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+    
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
